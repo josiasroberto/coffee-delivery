6f3d523e51b5b35ffc0de97df7678cd9aafde455
Josias Roberto <josiasroberto1@gmail.com>
2023-07-29 10:04:13 -0300

    First commit, config project

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  :focus-visible {
+    outline: 0;   
+  }
+
+  body{
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+    -wekbit-font-smoothing: antialiased;
+  }
+
+  body, input, button{
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 0.875rem;
+  }
+
+`
